3830501981029d547101936912e8319fa7775725
simba1949 <simba1949@outlook.com>
2023-11-16 22:53:07 +0800

    feature：使用 spring-jdbc 基于注解和xml实现声明式事务

---
+++ spring-transaction/spring-transaction-annotation/src/main/java/vip/openpark/transaction/annotation/domain/UserDO.java
@@ -0,0 +1,44 @@
+package vip.openpark.transaction.annotation.domain;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @author anthony
+ * @version 2023/11/6 17:16
+ */
+@Data
+public class UserDO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -5786786308237386825L;
+
+    private Long id;
+    private String code;
+    private String username;
+    private String realName;
+    private String nickName;
+    private String password;
+    private Byte gender;
+    private LocalDateTime birthday;
+    private String nation;
+    private String countryName;
+    private String idCard;
+    private String address;
+    private String phone;
+    private String email;
+
+    private Byte blEnable;
+    private Byte blDelete;
+    private Long version;
+    private LocalDateTime gmtCreate;
+    private String creator;
+    private Long creatorId;
+    private String creatorCode;
+    private LocalDateTime gmtModify;
+    private String modifier;
+    private Long modifierId;
+    private String modifierCode;
+}
