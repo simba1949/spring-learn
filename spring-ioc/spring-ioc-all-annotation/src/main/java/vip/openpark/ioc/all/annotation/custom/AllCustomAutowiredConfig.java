cf8dbf8725b33012fd908f7c5e54e29a84734f2d
anthony <anthony1949@zto.com>
2023-12-08 18:12:22 +0800

    feature：完全自定义 Autowired，第二种方式

--- spring-ioc/spring-ioc-all-annotation/src/main/java/vip/openpark/ioc/all/annotation/custom/AllCustomAutowiredConfig.java
+++ spring-ioc/spring-ioc-all-annotation/src/main/java/vip/openpark/ioc/all/annotation/custom/AllCustomAutowiredConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 import java.lang.annotation.Annotation;
 import java.util.Set;
@@ -19,12 +21,33 @@ import static org.springframework.context.annotation.AnnotationConfigUtils.AUTOW
 @Configuration
 public class AllCustomAutowiredConfig {
 
-	@Bean(name = AUTOWIRED_ANNOTATION_PROCESSOR_BEAN_NAME)
+	/**
+	 * 自定义注解和 @Autowired 都生效
+	 * 第一种方式：
+	 *
+	 * @return beanPostProcessor
+	 */
+//	@Bean(name = AUTOWIRED_ANNOTATION_PROCESSOR_BEAN_NAME)
+//	public static AutowiredAnnotationBeanPostProcessor beanPostProcessor() {
+//		AutowiredAnnotationBeanPostProcessor beanPostProcessor = new AutowiredAnnotationBeanPostProcessor();
+//		// 保证自定义注解和 @Autowired 都生效
+//		Set<Class<? extends Annotation>> collect = Stream.of(Autowired.class, AllCustomAutowired.class).collect(Collectors.toSet());
+//		beanPostProcessor.setAutowiredAnnotationTypes(collect);
+//		return beanPostProcessor;
+//	}
+
+	/**
+	 * 自定义注解和 @Autowired 都生效
+	 * 【推荐】
+	 * 第二种方式：使用 @Order(value = Ordered.LOWEST_PRECEDENCE - 3)
+	 *
+	 * @return beanPostProcessor
+	 */
+	@Bean
+	@Order(value = Ordered.LOWEST_PRECEDENCE - 3)
 	public static AutowiredAnnotationBeanPostProcessor beanPostProcessor() {
 		AutowiredAnnotationBeanPostProcessor beanPostProcessor = new AutowiredAnnotationBeanPostProcessor();
-		// 保证自定义注解和 @Autowired 都生效
-		Set<Class<? extends Annotation>> collect = Stream.of(Autowired.class, AllCustomAutowired.class).collect(Collectors.toSet());
-		beanPostProcessor.setAutowiredAnnotationTypes(collect);
+		beanPostProcessor.setAutowiredAnnotationType(AllCustomAutowired.class);
 		return beanPostProcessor;
 	}
 }
