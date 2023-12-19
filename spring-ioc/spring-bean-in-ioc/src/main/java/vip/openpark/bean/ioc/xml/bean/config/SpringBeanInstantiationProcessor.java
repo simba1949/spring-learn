57d285c5637a273359f5f9b6edd526ed8f7cd656
anthony <anthony1949@zto.com>
2023-12-19 14:50:28 +0800

    feature：spring bean 在实例化之【前后】进行后处理器操作

---
+++ spring-ioc/spring-bean-in-ioc/src/main/java/vip/openpark/bean/ioc/xml/bean/config/SpringBeanInstantiationProcessor.java
@@ -0,0 +1,50 @@
+package vip.openpark.bean.ioc.xml.bean.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.util.ObjectUtils;
+import vip.openpark.bean.ioc.xml.bean.BookBeanWithXMLInstantiationProcessor;
+
+/**
+ * @author anthony
+ * @version 2023/12/19 14:33
+ */
+public class SpringBeanInstantiationProcessor implements InstantiationAwareBeanPostProcessor {
+	
+	/**
+	 * 在实例化之前进行后处理器操作
+	 *
+	 * @param beanClass 需要实例化的类
+	 * @param beanName  需要实例化的 bean 的名称
+	 * @return 返回处理后的对象
+	 * @throws BeansException 抛出BeansException异常
+	 */
+	@Override
+	public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+		if (ObjectUtils.nullSafeEquals("bookBeanWithInstantiationProcessor", beanName) &&
+			    BookBeanWithXMLInstantiationProcessor.class.equals(beanClass)) {
+			// 把配置完成 BookBeanWithInstantiationProcessor Bean 覆盖
+			return new BookBeanWithXMLInstantiationProcessor();
+		}
+		// 保持 spring ioc 容器的实例化操作
+		return null;
+	}
+	
+	/**
+	 * 在对象实例化后进行后处理操作
+	 *
+	 * @param bean     实例化的对象
+	 * @param beanName 实例化的对象的名称
+	 * @return 布尔值，表示是否继续执行后续的bean后处理操作
+	 * @throws BeansException Bean后处理异常
+	 */
+	@Override
+	public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+		if (ObjectUtils.nullSafeEquals("bookBeanWithInstantiationProcessor", beanName) &&
+			    BookBeanWithXMLInstantiationProcessor.class.equals(bean.getClass())) {
+			// "bookBeanWithInstantiationProcessor" 对象不允许属性赋值（配置元信息——>属性值）
+			return false;
+		}
+		return InstantiationAwareBeanPostProcessor.super.postProcessAfterInstantiation(bean, beanName);
+	}
+}
---
+++ spring-ioc/spring-bean-in-ioc/src/test/java/vip/openpark/bean/ioc/xml/bean/BookBeanWithXMLInstantiationProcessorTest.java
@@ -0,0 +1,40 @@
+package vip.openpark.bean.ioc.xml.bean;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import vip.openpark.bean.ioc.xml.bean.config.SpringBeanInstantiationProcessor;
+
+/**
+ * @author anthony
+ * @version 2023/12/19 14:42
+ */
+@Slf4j
+public class BookBeanWithXMLInstantiationProcessorTest {
+	
+	/**
+	 * 1.创建 spring ioc 默认容器 DefaultListableBeanFactory
+	 * 2.使用 XmlBeanDefinitionReader 读取 xml 配置文件，解析成 beanDefinition
+	 */
+	@Test
+	public void runSpringIocTest() {
+		// 创建 spring ioc 容器，spring ioc 默认 DefaultListableBeanFactory
+		DefaultListableBeanFactory defaultListableBeanFactory = new DefaultListableBeanFactory();
+		// 添加自定义的 InstantiationAwareBeanPostProcessor
+		defaultListableBeanFactory.addBeanPostProcessor(new SpringBeanInstantiationProcessor());
+		
+		// 读取使用 XML 配置的 bean
+		XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(defaultListableBeanFactory);
+		String location = "classpath:bean.xml";
+		// 加载 XML 配置，返回配置 bean 的数量
+		int beanDefinitionCount = xmlBeanDefinitionReader.loadBeanDefinitions(location);
+		log.info("{}", beanDefinitionCount);
+		
+		UserBeanWithXml userBean = defaultListableBeanFactory.getBean("userBeanWithXml", UserBeanWithXml.class);
+		log.info("userBean = {}", userBean);
+		
+		BookBeanWithXMLInstantiationProcessor bookBean = defaultListableBeanFactory.getBean("bookBeanWithInstantiationProcessor", BookBeanWithXMLInstantiationProcessor.class);
+		log.info("bookBean = {}", bookBean);
+	}
+}
